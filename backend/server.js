508bc1ea2c2b430f3a5aecc384aaa1f64b563e4a
KetanKumar3 <ketanaryan2004@gmail.com>
2025-12-24 16:23:03 +0530

    feat: changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,17 @@ dotenv.config()
 
 connectDB()
 
-app.use(cors({
-    origin:["https://dulcet-tarsier-efe749.netlify.app"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials:true
-}))
+const corsOptions = {
+  origin: "https://dulcet-tarsier-efe749.netlify.app",
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
+
 
 app.use("/uploads", express.static("uploads"));
 
